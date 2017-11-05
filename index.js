9c0553294d613dff2890efdcd893020b17fab05b
sevy07 <sevy07@msn.com>
2017-11-05 14:29:56 +0000

    works with one image

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var im = require('imagemagick');
 
-var inputPath = 'path/to/input';
-var outputPath = 'path/to/output';
+var inputPath = './input/proposal-maria-wild.png';
+var outputPath = './output/proposal-maria-wild.png';
 var width = 300; // output width in pixels
 
 var args = [
@@ -12,21 +12,32 @@ var args = [
   'filter:support=2',
   '-thumbnail',
   width,
-  '-unsharp 0.25x0.25+8+0.065',
-  '-dither None',
-  '-posterize 136',
-  '-quality 82',
-  '-define jpeg:fancy-upsampling=off',
-  '-define png:compression-filter=5',
-  '-define png:compression-level=9',
-  '-define png:compression-strategy=1',
-  '-define png:exclude-chunk=all',
-  '-interlace none',
-  '-colorspace sRGB',
-  '-strip',
+  '-unsharp',
+  '0.25x0.08+8.3+0.045',
+  '-dither',
+  'None',
+  '-posterize',
+  '136',
+  '-quality',
+  '82',
+  '-define',
+  'jpeg:fancy-upsampling=off',
+  '-define',
+  'png:compression-filter=5',
+  '-define',
+  'png:compression-level=9',
+  '-define',
+  'png:compression-strategy=1',
+  '-define',
+  'png:exclude-chunk=all',
+  '-interlace',
+  'none',
+  '-colorspace',
+  'sRGB',
   outputPath
 ];
 
 im.convert(args, function (err, stdout, stderr) {
-  // do stuff
+  if (err) throw err;
+  console.log('stdout:', stdout);
 });
